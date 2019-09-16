a99522250a4dfd7c903299669347a0a67460633f
Ekta <ekta@innocirc.com>
2019-09-16 12:02:55 +0530

    Commit

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,3 +1,13 @@
+/*Class not required as kotlin internally created a class during runtime for JVM to execute the program*/
 fun main(args: Array<String>) {
-    println("Hello Kotlin ${args.size}")
+    var helloWorld = "Hello World" // Variables assume the datatype of it's first initialization
+    var myDecimal = 1.2
+    var myInt = 4
+    var myString: String //Mutable string
+    myString = "Hello Kotlin" // Can declare the type specifically
+    myString = "My First Kotlin Program"
+    val myConstantString: String //Immutable string
+    myConstantString = "This is a constant" //You can specifically declare the type
+    val myConstantInt = 34;  // val keyword for constants, can't be re-assigned
+    println(myString + "\n" + myDecimal + "\n" + myInt) //comment inline
 }
