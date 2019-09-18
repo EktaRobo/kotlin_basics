cb027bd8de2c5dc844e12e36d6f23c855171b945
Ekta <ekta@innocirc.com>
2019-09-18 12:54:24 +0530

    Loops in kotlin - labelled for loops

--- src/Hello.kt
+++ src/Hello.kt
@@ -3,36 +3,54 @@ import java.util.function.Consumer
 
 /*Class not required as kotlin internally created a class during runtime for JVM to execute the program*/
 fun main(args: Array<String>) {
-    var a = 5
-    var b = 3
+    //Loops in Kotlin
 
-    //If as expression
-    var maxVal = if (a > b) {
-        println("The max value is $a")
-        a
-    } else {
-        println("The max value is $b")
-        b
+    //For
+    for (i in 10 downTo 1) {
+        print("$i \t")
     }
-
-    println(maxVal)
-
-    when (maxVal) { //Substitute for switch case
-        in 1..10 -> println("The max value is in between 1 and 10")
-        10 -> println("The max value is 10")
-        a -> println("The max value is a: $a")
-        b -> println("The max value is b: $b")
-        else -> {
-            println("I don't know what x is")
+    println()
+    for (i in 1..10) {
+        if (i % 2 == 0)
+            print("$i \t")
+    }
+    println()
+    //Labelled for loop
+    firstFor@ for (i in 1..3) {
+        for (j in 1..3) {
+            print("$i $j\t\t")
+            if (i == 2 && j == 2)
+                break@firstFor //Break statement with labelled for loop
         }
     }
 
-    var maxValString = when (maxVal) { //when as expression
-        a -> "The max value is a: $a"
-        b -> "The max value is b: $b"
-        else -> "I don't know what x is"
+    println()
+    //Labelled for loop
+    firstFor@ for (i in 1..3) {
+        for (j in 1..3) {
+            if (i == 2 && j == 2)
+                continue@firstFor //Continue statement with labelled for loop
+            print("$i $j\t\t")
+        }
     }
 
-    println(maxValString)
+    println()
+    //While
+    var i =1
+    while (i <= 10) {
+        if (i % 2 == 0)
+            print("$i \t")
+        i++
+    }
+    println()
+    //Do while
+    i = 1
+    do {
+        print("$i \t")
+        i++
+    } while (i <= 10)
+
+
+
 }
 
