383d927e9c67e96ddd0f49edf3438168a8754fa4
Ekta <ekta@innocirc.com>
2019-09-18 13:13:02 +0530

    Function with parameters

--- src/explore/Explore.kt
+++ src/explore/Explore.kt
@@ -6,15 +6,20 @@ fun main(args: Array<String>) {
     println("The person's name is : $person.display()");
     println("The person's name is : ${person.returnString()}");
     println("The person's name is : ${person.display()}");
+    println("The person's name is : ${person.add(2, 6)}");
 }
 
 class Person {
-    var name :String = ""
-    fun returnString():String {
+    var name: String = ""
+    fun returnString(): String {
         return name; //comment inline
     }
 
     fun display() {
 
+    }
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
     }
 }
