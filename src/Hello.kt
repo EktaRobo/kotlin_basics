49af67ed6039131efb6215638effb4b6da2c666d
Ekta <ekta@innocirc.com>
2019-09-19 11:35:23 +0530

    Function as parameters

--- src/Hello.kt
+++ src/Hello.kt
@@ -3,36 +3,12 @@ import java.util.function.Consumer
 
 /*Class not required as kotlin internally created a class during runtime for JVM to execute the program*/
 fun main(args: Array<String>) {
-    var a = 5
-    var b = 3
-
-    //If as expression
-    var maxVal = if (a > b) {
-        println("The max value is $a")
-        a
-    } else {
-        println("The max value is $b")
-        b
-    }
-
-    println(maxVal)
-
-    when (maxVal) { //Substitute for switch case
-        in 1..10 -> println("The max value is in between 1 and 10")
-        10 -> println("The max value is 10")
-        a -> println("The max value is a: $a")
-        b -> println("The max value is b: $b")
-        else -> {
-            println("I don't know what x is")
-        }
-    }
-
-    var maxValString = when (maxVal) { //when as expression
-        a -> "The max value is a: $a"
-        b -> "The max value is b: $b"
-        else -> "I don't know what x is"
-    }
-
-    println(maxValString)
+    println("The Max Value among 3 and 5 is ${maxVal(3, 5)}")
 }
+
+fun maxVal(a: Int, b: Int): Int = if (a > b) { // If it is a single line, no need of curly braces
+    a // The last line is always returned and must be of the return type of the function
+} else {
+    b
+} //functions as expressions
 
