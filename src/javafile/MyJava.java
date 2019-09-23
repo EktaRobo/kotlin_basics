3f8090eab5e81448e38ffda8dc3f1578d5630d9d
Ekta <ekta@innocirc.com>
2019-09-23 16:04:31 +0530

    Hacker Earth Programming questions

--- src/javafile/MyJava.java
+++ src/javafile/MyJava.java
@@ -1,15 +1,58 @@
 package javafile;
 
-import kotlinfile.MyKotlin;
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
 
 public class MyJava {
     public static void main(String[] args) {
-        System.out.println("The Max Value among 3 and 5 is " + MyKotlin.maxVal(3, 5));
-        System.out.println("The volume is " + MyKotlin.findVolume(3, 5)); //Default function
+        /*System.out.println("The Max Value among 3 and 5 is " + MyKotlin.maxVal(3, 5));
+        System.out.println("The volume is " + MyKotlin.findVolume(3, 5)); //Default function*/
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+//        int num = 14531;
+        MyJava myJava = new MyJava();
+        if (myJava.isPalindrome(String.valueOf(num))) {
+            System.out.println(num);
+        } else {
+            System.out.println(myJava.closestPalindrome(num));
+        }
 
     }
 
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    private boolean isPalindrome(String num) {
+        char[] numArray = num.toCharArray();
+        for (int i = 0; i < numArray.length / 2; i++)
+            if (numArray[i] != numArray[numArray.length - 1 - i])
+                return false;
+        return true;
+    }
+
+    // function return closest Palindrome number
+    private int closestPalindrome(int num) {
+
+        // case1 : largest palindrome number
+        // which is smaller to given number
+        int RPNum = num - 1;
+
+        while (!isPalindrome(String.valueOf(abs(RPNum))))
+            RPNum--;
+
+        // Case 2 : smallest palindrome number
+        // which is greater than given number
+        int SPNum = num + 1;
+
+        while (!isPalindrome(String.valueOf(SPNum)))
+            SPNum++;
+
+        // check absolute difference
+        if (abs(num - RPNum) > abs(num - SPNum))
+            return SPNum;
+        else
+            return RPNum;
     }
 }
---
+++ src/javafile/OptimalMergePatterns.java
@@ -0,0 +1,63 @@
+package javafile;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class OptimalMergePatterns {
+    // Function to find minimum computation
+    static int minComputation(int size, int files[]) {
+
+        // create a min heap
+        PriorityQueue<Integer> pq
+                = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < size; i++) {
+
+            // add sizes to priorityQueue
+            pq.add(files[i]);
+        }
+
+        // variable to count total computations
+        int count = 0;
+
+        while (pq.size() > 1) {
+
+            // pop two smallest size element
+            // from the min heap
+            int temp = pq.poll() + pq.poll();
+
+            // add the current computations
+            // with the previous one's
+            count += temp;
+
+            // add new combined file size
+            // to priority queue or min heap
+            pq.add(temp);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        // no of files
+//        int size = 3;
+
+        // 6 files with their sizes
+
+        Scanner dd = new Scanner(System.in);
+
+        int size = dd.nextInt();
+        int files[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            files[i] = dd.nextInt();
+        }
+
+            // total no of computations
+            // do be done final answer
+            System.out.println(minComputation(size, files));
+    }
+}
+
